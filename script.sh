cd61922de7b73d03b9ceb796d34252976e838a9f
Andreii Kachanov <andreii.kachanov@gmail.com>
2021-02-17 00:33:06 +0200

    checking exists folder destination

--- script.sh
+++ script.sh
@@ -6,25 +6,30 @@ dest_folders=(
 "/home/andrey/destination1/"
 "/home/andrey/destination2/"
 "/home/andrey/destination3/"
+"/home/andrey/destination4/"
+"/home/andrey/destination5/"
 )
 
 log=/home/andrey/rsync_watch.log
 
-inotifywait -m $source_dir -e close_write | # определяем, что в папку был добавлен новый файл, который был закрыт после записи в него данных
+inotifywait -m $source_dir -e close_write | # we define that a new file was added to the folder, which was closed after writing data
     while read dir action file; do
         echo "$(date +%d.%m.%Y\ %T). The file '$dir$file' appeared in via '$action'." >> $log
-        
-        for dest in "${dest_folders[@]}"; do 
-            echo $dest >> $log
 
-            rsync -z -c "$dir$file" $dest # копирование с проверкой контрольных сумм для файлов
-            if [ "$?" -eq "0" ]
+        for dest in "${dest_folders[@]}"; do
+            if [ -d "$dest" ]; #check if destination directory exists 
                 then
-                  echo "DONE: $(date +%d.%m.%Y\ %T).Copied file $dir$file to $dest." >> $log
+                    rsync -z -c "$dir$file" $dest #copy with checksum verification for files
+                    
+                    if [ "$?" -eq "0" ]
+                        then
+                            echo "DONE: $(date +%d.%m.%Y\ %T). Copied file $dir$file to $dest." >> $log
+                        else
+                            echo "ERROR: $(date +%d.%m.%Y\ %T). Error while running rsync. File - '$dir$file'." >> $log
+                    fi              
                 else
-                  echo "ERROR: $(date +%d.%m.%Y\ %T). Error while running rsync. File - '$dir$file'." >> $log
+                    echo "ERROR: $(date +%d.%m.%Y\ %T). $dest not found. Can not continue." >> $log
             fi
-
         done       
     done
 
